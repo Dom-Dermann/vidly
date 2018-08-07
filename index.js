1214286758ac71a16be2b476fbb7e739612a4672
Dom-Dermann <dominikdermann@gmail.com>
2018-08-07 20:31:21 +0200

    added general get

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+const genres = [
+    {
+        id: 1, 
+        genre: 'horror'
+    },
+    {
+        id: 2, 
+        genre: 'romance'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Vidly.com is a great server. Use our API to get info.');
-})
+});
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
 
 app.listen(3000, () => console.log('Listening on port 3000.'));
