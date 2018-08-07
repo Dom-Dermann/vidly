03015808f5641838edc4f6db320eb2eb4134c8ab
Dom-Dermann <dominikdermann@gmail.com>
2018-08-07 21:01:02 +0200

    post endpoint added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const genres = [
     {
@@ -18,6 +21,37 @@ app.get('/', (req, res) => {
 
 app.get('/api/genres', (req, res) => {
     res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find( (g) => g.id === parseInt(req.params.id));
+    if (!genre){
+        res.status(404).send('Genre not found.');
+        return;
+    }
+
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+    const schema = {
+        genre: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        res.status(400).send('Bad request: ' + result.error.details[0].message);
+        return;
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        genre: req.body.genre
+    }
+    genres.push(genre);
+
+    res.send(req.body.genre);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000.'));
