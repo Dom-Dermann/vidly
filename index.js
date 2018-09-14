5fa19457302033d33dd6243d92a7e910fdc6d38f
Dom-Dermann <dominikdermann@gmail.com>
2018-09-14 17:45:58 +0200

    auth endpoint added

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const customer = require('./routes/customers');
 const movie = require('./routes/movies');
 const rental = require('./routes/rentals');
 const user = require('./routes/users');
+const auth = require('./routes/auth');
 
 // install middleware
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use('/api/customers', customer);
 app.use('/api/movies', movie);
 app.use('/api/rentals', rental);
 app.use('/api/users', user);
+app.use('/api/auth', auth);
 
 // get the db host path
 const dbHost = config.get('Database.host');
---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const {User, validateUser, validatePasswordComplexity} = require('../models/user');
+const bcrypt = require('bcrypt');
+const _ = require('lodash'); // for object / array / string manipulation etc.
+
+router.post('/', async( req, res) => {
+    // validate user input
+    const result = validate(req.body);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
+    // make sure the user is not already registered by finding the unique email
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+    res.send(true);
+});
+
+
+function validate(body) {
+    const schema = {
+        email: Joi.string().required().min(5).max(255).email(),
+        password: Joi.string().min(5).max(30).required()
+    };
+    return Joi.validate(body, schema);
+}
+
+module.exports = router;
